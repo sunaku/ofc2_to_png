f19b62a7a5b946e36229ceb05d7c55647caa8d8b
Suraj N. Kurapati <sunaku@gmail.com>
2009-08-21 08:09:51 -0700

    render charts in parallel instead of serially
    move flash applet off-screen to improve performance

--- ofc2_to_png.rb
+++ ofc2_to_png.rb
@@ -4,7 +4,7 @@
 #
 # = Usage
 #
-#   ruby ofc2_to_png.rb <BROWSER> <WIDTH> <HEIGHT> <CHART...>
+#   ruby ofc2_to_png.rb <BROWSER> <THREADS> <WIDTH> <HEIGHT> <CHART...>
 #
 #   For each given <CHART>, a corresponding <CHART>.png file is written.
 #
@@ -12,6 +12,8 @@
 #
 #   BROWSER:: Shell command for launching a browser that supports Flash 9+
 #             and whose process can be killed without external consequences.
+#
+#   THREADS:: Number of Flash applets the browser can tolerate at a time.
 #
 #   WIDTH::   Width of a rendered PNG image (measured in pixels).
 #
@@ -35,8 +37,8 @@ require 'haml'
 
 ERRORS = []
 
-raise 'not enough arguments' if ARGV.length < 4
-BROWSER, WIDTH, HEIGHT, *FILES = ARGV
+raise 'not enough arguments' if ARGV.length < 5
+BROWSER, THREADS, WIDTH, HEIGHT, *FILES = ARGV
 
 # launch the browser as a subprocess
 BROWSER_PID = Thread.new do
@@ -98,91 +100,97 @@ __END__
       - chart_id = "chart_#{num}"
       - chart_url = "/chart/#{num}"
 
-      .chart{:url => chart_url, :num => num}
+      .chart{:url => chart_url, :num => num, :pending => true}
+        = "Chart #{num}: #{FILES[num]}"
+
+        - # placeholder for the flash applet
         .flash{:id => chart_id}
-          = "Chart #{num}: #{FILES[num]}"
 
     :javascript
-      var chart = null;
+      function process_chart(chart) {
+        chart.removeAttr('pending').css('font-weight', 'bold');
 
-      // instantiate the next flash chart
-      function setup() {
-        chart = $('.chart:first');
+        //
+        // instantiate the flash applet
+        //
+        var flash = $('.flash', chart);
 
-        if (chart.length) {
-          var flash = $('.flash', chart);
+        swfobject.embedSWF(
+          'open-flash-chart.swf', flash.attr('id'), #{WIDTH}, #{HEIGHT},
+          '9.0.0', false, {'data-file': chart.attr('url')}, false, false,
+          function(event) {
+            var flash = event.ref;
 
-          // instantiate the flash chart
-          swfobject.embedSWF(
-            'open-flash-chart.swf',
-            flash.attr('id'),
-            #{WIDTH}, #{HEIGHT},
-            '9.0.0', false,
-            {'data-file': chart.attr('url')}
-          );
+            //
+            // move flash applet off-screen to improve browser performance
+            //
+            $(flash).css({
+              position: 'absolute',
+              top: -#{HEIGHT.to_i + 10},
+              left: -#{WIDTH.to_i + 10}
+            });
+
+            //
+            // wait until the chart animation settles before rendering image
+            //
+            var SAMPLE_DELAY = 200, MIN_STABLE_SAMPLES = 3;
+            var prev_sample = null, num_stable_samples = 0;
+
+            function wait_for_stability() {
+              if (flash.get_img_binary) {
+                var curr_sample = null;
+
+                try {
+                  curr_sample = flash.get_img_binary();
+                }
+                catch(error) {
+                  $.post('/error', {'error': error, 'num': chart.attr('num')});
+                }
+
+                if (prev_sample == curr_sample) {
+                  num_stable_samples++;
+
+                  if (num_stable_samples >= MIN_STABLE_SAMPLES) {
+                    //
+                    // chart is stable now, we can render & upload
+                    //
+                    $.post(chart.attr('url'), {'image': curr_sample});
+
+                    chart.remove();
+                    process_next_chart();
+
+                    return;
+                  }
+                }
+                else {
+                  num_stable_samples = 0;
+                }
+
+                prev_sample = curr_sample;
+              }
+
+              setTimeout(wait_for_stability, SAMPLE_DELAY); // loop
+            }
+
+            wait_for_stability();
+          }
+        );
+      }
+
+      function process_next_chart() {
+        var next_chart = $('.chart[pending]:first');
+        if (next_chart.length) {
+          process_chart(next_chart);
         }
         else {
           $('body').text('You may close this window now.');
-          $.get('/end'); // end loop: notify server about completion
+          $.get('/end'); // notify server about completion
         }
       }
 
-      // OFC2 callback for the flash chart instantiated in render()
-      function ofc_ready() {
-        var flash = $('[id^=chart_]', chart).get(0);
-
-        flash.render = function() {
-          try {
-            return this.get_img_binary();
-          }
-          catch(error) {
-            $.post('/error', {'error': error, 'num': chart.attr('num')});
-            return null;
-          }
+      $(function() {
+        for (var i = 0; i < #{THREADS}; i++) {
+          process_next_chart();
         }
-
-        //
-        // wait until the chart animation settles
-        //
-        var SAMPLE_DELAY = 250, MIN_STABLE_SAMPLES = 3;
-        var prev_sample = null, num_stable_samples = 0;
-
-        function wait_for_stability() {
-          var curr_sample = flash.render();
-
-          if (prev_sample == curr_sample) {
-            num_stable_samples++;
-
-            if (num_stable_samples >= MIN_STABLE_SAMPLES) {
-              num_stable_samples = 0;
-              upload(curr_sample);
-              return;
-            }
-          }
-          else {
-            num_stable_samples = 0;
-          }
-
-          prev_sample = curr_sample;
-          setTimeout(wait_for_stability, SAMPLE_DELAY); // loop
-        }
-
-        wait_for_stability();
-      }
-
-      // uploads the given image data to the
-      // server and proceeds to the next chart
-      function upload(image_data) {
-        $.ajax({
-          type: 'POST',
-          url: chart.attr('url'),
-          data: {'image': image_data},
-          complete: function() {
-            chart.remove();
-            setup(); // continue loop: render the next chart
-          }
-        });
-      }
-
-      $(setup); // begin loop: render the first chart
+      });
 
